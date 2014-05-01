020156525b5b55def325c9f75ecf95da322035ea
Erk EKİN <erkekin@gmail.com>
2014-05-01 21:30:48 +0300

    Initial Commit

---
+++ Genetic/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Genetic
+//
+//  Created by Erk EKİN on 01/05/14.
+//  Copyright (c) 2014 erkekin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GeneticTests/GeneticTests.m
@@ -0,0 +1,34 @@
+//
+//  GeneticTests.m
+//  GeneticTests
+//
+//  Created by Erk EKİN on 01/05/14.
+//  Copyright (c) 2014 erkekin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GeneticTests : XCTestCase
+
+@end
+
+@implementation GeneticTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
